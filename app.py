80a685d621a0300ec933a4e78ef3ac9986d8099a
rojoyin <rojoyin@gmail.com>
2022-04-09 00:33:44 -0500

    Add predicitions endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 from sklearn import linear_model
 import pickle
@@ -9,14 +9,16 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("Model app")
 
+model = pickle.load(open("model/model.pkl", "rb"))
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello world</p>"
 
 
-@app.route("/train")
-def train_model():
+@app.route("/retrain")
+def regenerate_train_model():
     logger.info("Reading csv file")
     training_df = pd.read_csv("model/training.csv")
     # predictors independent variables
@@ -29,6 +31,19 @@ def train_model():
     logger.info("Saving model as pkl file")
     pickle.dump(lm, open("model/model.pkl", "wb"))
     return render_template("trained.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    body = request.json
+    if "Rooms" not in list(body.keys()) or "Distance" not in list(body.keys()):
+        return "Bad request, include 'Rooms' or 'Distance'", 400
+
+    rooms = body["Rooms"]
+    distance = body["Distance"]
+    prediction = model.predict([[rooms, distance]])
+    output = round(prediction[0], 2)
+    return {"prediction": output}, 200
 
 
 if __name__ == "__main__":
