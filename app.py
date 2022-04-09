6dedbd2b74d115efbafa06262479758b5f6d90c8
rojoyin <rojoyin@gmail.com>
2022-04-08 22:05:39 -0500

    Add endpoint to create model

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+import pandas as pd
+from sklearn import linear_model
+import pickle
 
 app = Flask(__name__)
 
@@ -6,6 +9,18 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello world</p>"
+
+
+@app.route("/train")
+def train_model():
+    training_df = pd.read_csv("model/training.csv")
+    # predictors independent variables
+    predictors = training_df[["Rooms", "Distance"]]
+    # outcomes dependent variables
+    outcomes = training_df["Value"]
+    lm = linear_model.LinearRegression()
+    lm.fit(predictors, outcomes)
+    pickle.dump(lm, open("model/model.pkl", "wb"))
 
 
 if __name__ == "__main__":
